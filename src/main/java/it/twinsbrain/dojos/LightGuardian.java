98136a8e0d8f5327793e44bde20dd2496568c4e4
Paolo Sciarra <itbhps@gmail.com>
2023-12-22 21:28:33 +0100

    refactored removed duplication

--- src/main/java/it/twinsbrain/dojos/LightGuardian.java
+++ src/main/java/it/twinsbrain/dojos/LightGuardian.java
@@ -1,47 +1,69 @@
 package it.twinsbrain.dojos;
 
-
+import it.twinsbrain.dojos.commands.Command;
 import it.twinsbrain.dojos.commands.ToggleCommand;
 import it.twinsbrain.dojos.commands.TurnOffCommand;
 import it.twinsbrain.dojos.commands.TurnOnCommand;
 import it.twinsbrain.dojos.values.From;
 import it.twinsbrain.dojos.values.To;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LightGuardian {
   private final Pattern turnOnPattern = Pattern.compile("turn on (\\d),(\\d) through (\\d),(\\d)");
-  private final Pattern turnOffPattern = Pattern.compile("turn off (\\d),(\\d) through (\\d),(\\d)");
+  private final Pattern turnOffPattern =
+      Pattern.compile("turn off (\\d),(\\d) through (\\d),(\\d)");
   private final Pattern togglePattern = Pattern.compile("toggle (\\d),(\\d) through (\\d),(\\d)");
   private final LightGrid lightGrid = new LightGrid();
-
-  public void receive(String commandString) {
-    var turnOnMatcher = turnOnPattern.matcher(commandString);
-    if (turnOnMatcher.matches()) {
-      var x1 = Integer.parseInt(turnOnMatcher.group(1));
-      var y1 = Integer.parseInt(turnOnMatcher.group(2));
-      var x2 = Integer.parseInt(turnOnMatcher.group(3));
-      var y2 = Integer.parseInt(turnOnMatcher.group(4));
-      lightGrid.accept(new TurnOnCommand(From.of(x1, y1), To.of(x2, y2)));
-    }
-    var turnOffMatcher = turnOffPattern.matcher(commandString);
-    if (turnOffMatcher.matches()) {
-      var x1 = Integer.parseInt(turnOffMatcher.group(1));
-      var y1 = Integer.parseInt(turnOffMatcher.group(2));
-      var x2 = Integer.parseInt(turnOffMatcher.group(3));
-      var y2 = Integer.parseInt(turnOffMatcher.group(4));
-      lightGrid.accept(new TurnOffCommand(From.of(x1, y1), To.of(x2, y2)));
-    }
-    var toggleMatcher = togglePattern.matcher(commandString);
-    if (toggleMatcher.matches()) {
-      var x1 = Integer.parseInt(toggleMatcher.group(1));
-      var y1 = Integer.parseInt(toggleMatcher.group(2));
-      var x2 = Integer.parseInt(toggleMatcher.group(3));
-      var y2 = Integer.parseInt(toggleMatcher.group(4));
-      lightGrid.accept(new ToggleCommand(From.of(x1, y1), To.of(x2, y2)));
-    }
-  }
 
   public int howManyLightsOn() {
     return lightGrid.numberOfLightsOn();
   }
+
+  public void receive(String commandString) {
+    var turnOnCommandMatcher = createCommandMatcher(turnOnPattern, TurnOnCommand::new);
+    var turnOffCommandMatcher = createCommandMatcher(turnOffPattern, TurnOffCommand::new);
+    var toggleCommandMatcher = createCommandMatcher(togglePattern, ToggleCommand::new);
+    var chain = List.of(turnOffCommandMatcher, turnOnCommandMatcher, toggleCommandMatcher);
+    var command = chain.stream()
+            .map(commandMatcher -> commandMatcher.match(commandString))
+            .filter(Optional::isPresent)
+            .findFirst()
+            .flatMap(Function.identity())
+            .orElse(null);
+    lightGrid.accept(Objects.requireNonNull(command));
+  }
+
+  private CommandMatcher createCommandMatcher(Pattern pattern, CommandFactory factory) {
+    return commandString -> {
+      var turnOnMatcher = pattern.matcher(commandString);
+      if (turnOnMatcher.matches()) {
+        var fromToPair = coordinates(turnOnMatcher);
+        return Optional.of(factory.create(fromToPair.first, fromToPair.second));
+      } else {
+        return Optional.empty();
+      }
+    };
+  }
+
+  private static Pair<From, To> coordinates(Matcher matcher) {
+    var x1 = Integer.parseInt(matcher.group(1));
+    var y1 = Integer.parseInt(matcher.group(2));
+    var x2 = Integer.parseInt(matcher.group(3));
+    var y2 = Integer.parseInt(matcher.group(4));
+    return new Pair<>(From.of(x1, y1), To.of(x2, y2));
+  }
+  private interface CommandFactory {
+    Command create(From from, To to);
+  }
+  private interface CommandMatcher {
+    Optional<Command> match(String commandString);
+
+  }
+
+  private record Pair<T, U>(T first, U second) {}
 }
